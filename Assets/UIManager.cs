c19ccc4d2a9c07d2ed8a11036004f50aebd11559
fmDevart <mesuracafrancesco2@gmail.com>
2023-08-15 15:19:59 +0200

    minor fix on UI Manager

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -2,63 +2,90 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
 
     public GameObject toSpawn;
-    private GameObject spawned;
+    public GameObject spawned;
     private Cards cards;
     public Card cardToShow;
-    private int index = 0;
+    public int index = 0;
+    public Transform bodyPanel;
+    public Transform titlePanel;
+    public TMP_Text textComponent;
+    private TMP_Text[] taboos;
+    public string titleToShow;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(GameManager.instance.GetCards() != null)
+        if (GameManager.instance.GetCards() != null)
         {
             this.cards = GameManager.instance.GetCards();
         }
 
-        if(this.cards != null)
+        if (this.cards != null)
         {
-            
-                this.cardToShow =  this.cards.cards[index];
 
-            if(!spawned)
+            this.cardToShow = this.cards.cards[index];
+            titleToShow = cardToShow.obj;
+
+            if (!spawned)
             {
                 spawned = Instantiate(toSpawn, transform);
             }
 
 
-            Transform titlePanel = spawned.transform.GetChild(0);
-            Transform bodyPanel = spawned.transform.GetChild(1);
+            titlePanel = spawned.transform.GetChild(0);
+            bodyPanel = spawned.transform.GetChild(1);
 
-            Text textComponent = titlePanel.GetComponentInChildren<Text>();
+            textComponent = titlePanel.GetComponentInChildren<TMP_Text>();
 
-            // Modifica il testo nel componente di testo
-            if (textComponent != null)
-            {
-                textComponent.text = cardToShow.obj;
-            }
+
+            textComponent.text = cardToShow.obj;
+
+            
+
+
 
         }
- 
+
     }
     public void NextCard()
     {
-        this.index++;
-
+        if(index < this.cards.cards.Length -1)
+        {
+            this.index++;
+            Debug.Log("pressed next");
+        }
+        else
+        {
+            Debug.Log("no more cards");
+        }
     }
 
     public void PrevCard()
     {
-        this.index--;
+
+        if (index > 0)
+        {
+            Debug.Log("pressed Prev");
+            this.index--;
+        }
+        else
+        {
+            Debug.Log("no more cards prev");
+        }
+
+
+     
 
     }
 
